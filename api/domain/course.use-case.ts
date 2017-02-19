ebf4b3abcdf25ad20874c001c100a26d4fe91f60
Felipe Goncalves <felipe.goncalves@taqtile.com>
2017-02-19 17:53:04 -0300

    Added TypeDi as Dependency Injection Framewokr

--- api/domain/course.use-case.ts
+++ api/domain/course.use-case.ts
@@ -1,9 +1,12 @@
+import { Inject, Service } from "typedi";
 import { Course } from "./course";
 import { CourseGateway } from "./course.contract";
+import "../data/gateways/course.gateway.mongo";
 
+@Service()
 export class CourseUseCases {
 
-  constructor(private courseGateway: CourseGateway) {}
+  constructor(@Inject("course.gateway.mongo") private courseGateway: CourseGateway) {}
 
   getCourses(): Promise<Course[]> {
     return this.courseGateway.getAllCourses();
