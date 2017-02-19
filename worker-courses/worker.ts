9fef001950ac657576dceb6fa914cae4c22b3a1f
André <andre.spanguero@gmail.com>
2017-02-19 17:03:58 -0300

    Starting worker

---
+++ worker-courses/worker.ts
@@ -0,0 +1,10 @@
+import * as scheduler from 'node-schedule';
+
+import { GetAllCoursesUseCase } from './domain/get-all-courses'
+
+var getAllCoursesUseCase = new GetAllCoursesUseCase();
+
+let schedule = scheduler.scheduleJob('* * * * *', () =>
+{
+  getAllCoursesUseCase.getAllCourses();
+});
