b67b9557acb4d5156b1f690ced32d545380f40fa
Felipe Goncalves <felipe.goncalves@taqtile.com>
2017-02-28 21:10:52 -0300

    Solved some packages dependecies

--- worker-courses/models/course.ts
+++ worker-courses/models/course.ts
@@ -1,11 +1,6 @@
 export class Course {
-  public name: string;
-  public description: string;
-  public url: string;
 
-  constructor(name: string, description: string, url: string){
-    this.name = name;
-    this.description = description;
-    this.url = url;
-  }
+  constructor(public name: string,
+              public description: string,
+              public url: string) { }
 }
